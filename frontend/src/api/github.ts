674fbf98e1cd597e7670f226ce630f5d2b0e2989
Tommy Barnes <thomas.jonathan.barnes@gmail.com>
2021-10-14 15:06:45 -0400

    Fixed conflicts in github.ts and IngressWorkflow.tsx after merges.

--- frontend/src/api/github.ts
+++ frontend/src/api/github.ts
@@ -15,6 +15,7 @@ import {
   ListServicesRequest,
   ListWorkflowsEntryRequest,
   Repository,
+  RepoWorkflow,
   ServiceEntry,
   TreeEntry,
   Workflow,
@@ -23,67 +24,6 @@ import {
 
 const baseURL = "http://localhost:8080";
 axios.defaults.baseURL = baseURL;
-
-export function ListRepositories(
-  req: ListRepositoriesRequest,
-  handleResponse: (response: Repository[]) => void
-) {
-  console.log("ListRepositories");
-  axios.defaults.baseURL = baseURL;
-  axios
-    .get(baseURL + "/api/github/repositories", {
-      params: {
-        repoOwner: req.repoOwner,
-      },
-    })
-    .then((resp) => {
-      console.log("Got Response: ");
-      console.log(resp);
-      handleResponse(resp.data.data);
-    });
-}
-
-export function ListBranches(
-  req: ListBranchesRequest,
-  handleResponse: (response: Branch[]) => void
-) {
-  console.log("ListBranches");
-  axios.defaults.baseURL = baseURL;
-  axios
-    .get(baseURL + "/api/github/repository/branches", {
-      params: {
-        repoOwner: req.repoOwner,
-        repoName: req.repoName,
-      },
-    })
-    .then((resp) => {
-      console.log("Got Response: ");
-      console.log(resp);
-      handleResponse(resp.data.data);
-    });
-}
-
-export function ListCharts(
-  req: ListChartEntryRequest,
-  handleResponse: (response: ChartEntry[]) => void
-) {
-  console.log("ListCharts");
-  console.log(req);
-  axios.defaults.baseURL = baseURL;
-  axios
-    .get(baseURL + "/api/github/repository/charts", {
-      params: {
-        repoOwner: req.repoOwner,
-        repoName: req.repoName,
-        repoBranch: req.repoBranch,
-      },
-    })
-    .then((resp) => {
-      console.log("Got Response: ");
-      console.log(resp);
-      handleResponse(resp.data.data);
-    });
-}
 
 export function ListWorkflows(
   req: ListWorkflowsEntryRequest,
@@ -172,50 +112,31 @@ export function GetAction(
     });
 }
 
-export function ListServices(
-  req: ListServicesRequest,
-  handleResponse: (response: ServiceEntry[]) => void
-) {
-  console.log("ListServices");
+export function ListDirectories(req: ListDirectoriesRequest, handleResponse: (response: TreeEntry[]) => void) {
+  console.log("ListDirectories");
   console.log(req);
-  axios.defaults.baseURL = baseURL;
-  axios
-    .get(baseURL + "/api/github/repository/services", {
-      params: {
-        repoOwner: req.repoOwner,
-        repoName: req.repoName,
-        repoBranch: req.repoBranch,
-        chartPath: req.chartPath,
-      },
-    })
+
+  if (!req.repoOwner) {
+    console.warn("ListDirectories cancelled due to blank username")
+    return
+  }
+
+  axios.defaults.baseURL = baseURL
+  axios.get(baseURL + "/api/github/repository/chartdirectory", {
+    params: {
+      repoOwner: req.repoOwner,
+      repoName: req.repoName,
+      branchSha: req.branchSha,
+      chartPath: req.chartPath
+    }
+  })
     .then((resp) => {
-      console.log("Got Response: ");
-      console.log(resp);
+      console.log("Got Response: ")
+      console.log(resp)
       handleResponse(resp.data.data);
-    });
+    })
 }
 
-export function ListDirectories(
-  req: ListDirectoriesRequest,
-  handleResponse: (response: TreeEntry[]) => void
-) {
-  console.log("ListServices");
-  console.log(req);
-  axios.defaults.baseURL = baseURL;
-  axios
-    .get(baseURL + "/api/github/repository/chartdirectory", {
-      params: {
-        repoOwner: req.repoOwner,
-        repoName: req.repoName,
-        branchSha: req.branchSha,
-        chartPath: req.chartPath,
-      },
-    })
-    .then((resp) => {
-      console.log("Got Response: ");
-      console.log(resp);
-      handleResponse(resp.data.data);
-    });
 
 export function ListRepositories(req: ListRepositoriesRequest, handleResponse: (response: Repository[]) => void) {
   console.log("ListRepositories");
@@ -333,31 +254,6 @@ export function ListRepoWorkflows(req: ListRepoWorkflowRequest, handleResponse: 
     })
 }
 
-export function ListDirectories(req: ListDirectoriesRequest, handleResponse: (response: TreeEntry[]) => void) {
-  console.log("ListDirectories");
-  console.log(req);
-
-  if (!req.repoOwner) {
-    console.warn("ListDirectories cancelled due to blank username")
-    return
-  }
-
-  axios.defaults.baseURL = baseURL
-  axios.get(baseURL + "/api/github/repository/chartdirectory", {
-    params: {
-      repoOwner: req.repoOwner,
-      repoName: req.repoName,
-      branchSha: req.branchSha,
-      chartPath: req.chartPath
-    }
-  })
-    .then((resp) => {
-      console.log("Got Response: ")
-      console.log(resp)
-      handleResponse(resp.data.data);
-    })
-}
-
 export function CreateIngressPR(
   req: CreateIngressPRRequest,
   handleResponse: (prURL: string) => void
@@ -379,13 +275,13 @@ export function CreateIngressPR(
 }
 
 export function CreateWorkflowPR(req: CreateWorkflowPRRequest, handleResponse: (prURL: string) => void) {
-    console.log("CreateWorkflowPR");
-    console.log(req);
-    axios.defaults.baseURL = baseURL
-    axios.post(baseURL + '/api/github/repository/pr/workflow', req)
-        .then((resp) => {
-            console.log("Got Response: ")
-            console.log(resp)
-            handleResponse(resp.data.pullRequestURL);
-        })
+  console.log("CreateWorkflowPR");
+  console.log(req);
+  axios.defaults.baseURL = baseURL
+  axios.post(baseURL + '/api/github/repository/pr/workflow', req)
+    .then((resp) => {
+      console.log("Got Response: ")
+      console.log(resp)
+      handleResponse(resp.data.pullRequestURL);
+    })
 }
